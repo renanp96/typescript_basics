db0e917cb7da02477ef326222ca7bcf31fee05b8
renanp96 <renanpa1996@gmail.com>
2023-09-12 11:32:40 -0300

    refact: protegendo os dados e acesso as variaveis instanciadas na classe

--- app/models/negotiation.ts
+++ app/models/negotiation.ts
@@ -1,8 +1,14 @@
 export class Negotiation {
     constructor(
-        public readonly date: Date, 
-        public readonly qtde: number, 
-        public readonly value: number) {}
+        private readonly _date: Date,
+        public readonly qtde: number,
+        public readonly value: number
+    ) {}
+
+    get date(): Date {
+        const date = new Date(this._date.getTime());
+        return date;
+    }
 
     get volume(): number {
         return this.qtde * this.value;
--- dist/js/models/negotiation.js
+++ dist/js/models/negotiation.js
@@ -1,8 +1,12 @@
 export class Negotiation {
-    constructor(date, qtde, value) {
-        this.date = date;
+    constructor(_date, qtde, value) {
+        this._date = _date;
         this.qtde = qtde;
         this.value = value;
+    }
+    get date() {
+        const date = new Date(this._date.getTime());
+        return date;
     }
     get volume() {
         return this.qtde * this.value;
