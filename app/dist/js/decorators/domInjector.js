51bef8efaf06df90c8bde612bc39f518d501f955
renanp96 <renanpa1996@gmail.com>
2023-09-18 15:20:34 -0300

    fix: aplicando cache decorator copy

--- app/dist/js/decorators/domInjector.js
+++ app/dist/js/decorators/domInjector.js
@@ -1,8 +1,11 @@
 export function domInjector(selector) {
     return function (target, propertyKey) {
+        let element;
         const getter = function () {
-            const element = document.querySelector(selector);
-            console.log(`bucando elemento do DOM com o selector ${selector} para injetar em ${propertyKey}`);
+            if (!element) {
+                element = document.querySelector(selector);
+                console.log(`bucando elemento do DOM com o selector ${selector} para injetar em ${propertyKey}`);
+            }
             return element;
         };
         Object.defineProperty(target, propertyKey, { get: getter });
--- app/src/decorators/domInjector.ts
+++ app/src/decorators/domInjector.ts
@@ -1,9 +1,14 @@
 export function domInjector(selector: string) {
     return function (target: any, propertyKey: string) {
 
+        let element: HTMLElement;
+
         const getter = function () {
-            const element = document.querySelector(selector);
-            console.log(`bucando elemento do DOM com o selector ${selector} para injetar em ${propertyKey}`)
+            if(!element){
+                element = <HTMLElement> document.querySelector(selector);
+                console.log(`bucando elemento do DOM com o selector ${selector} para injetar em ${propertyKey}`)
+            }
+
             return element;
         }
 
